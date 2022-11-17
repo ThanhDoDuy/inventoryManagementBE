ac6e1ab6795f2420cf65936bb34f95fa66116b8d
hiep.thai <hiep.thai@menlosecurity.com>
2022-11-17 13:46:00 +0700

    done Back End

--- server.js
+++ server.js
@@ -2,12 +2,45 @@ const dotenv = require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cors = require('cors');
+const cors = require("cors");
+const userRoute = require("./routes/userRoute");
+const productRoute = require("./routes/productRoute");
+const contactRoute = require("./routes/contactRoute");
+const errorHandler = require("./middleWare/errorMiddleware");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// Middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    credentials: true,
+  })
+);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Routes Middleware
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/contactus", contactRoute);
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Home Page");
+  });
+  
+  // Error Middleware
+  app.use(errorHandler);
 // connect to DB and start server
 mongoose
     .connect(process.env.MONGOOSE_URL)
